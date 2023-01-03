d5ec9a31f071a1383b17b5df0e8ab9800da1c86e
Tnaike <timmynaike@gmail.com>
2023-01-03 17:27:07 +0100

    Modify pagination style and test

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         target='_blank'
         rel='noopener noreferrer'
       >
-        Learn React
+        TypeScript React Pagination
       </a>
       <div className='paginationWrap'>
         <Pagination
